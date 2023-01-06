b9f20105a36f607e3836b39ecd45da31acd663ae
김동일 <ehddlfwkd32@naver.com>
2023-01-06 18:53:32 +0900

    [fix] 게임방, 채팅 구현중

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+// const path = require('path');
 const http = require('http');
-const server =http.createServer(app);
-const PORT = process.env.PORT||3500;
+const server = http.createServer(app);
+
 const moment = require('moment')
 const cookieParser = require('cookie-parser');
-const cors = require("cors");
+// const cors = require("cors");
+
+const PORT = process.env.PORT || 5500;
+//소켓 부분
 const socketIO = require('socket.io');
 const io = socketIO(server, {
     path: '/socket.io',
@@ -16,33 +21,50 @@ const io = socketIO(server, {
 },
 );
 
-app.use(express.static(path.join(__dirname, 'src')))
+// app.use(express.static(path.join(__dirname, 'src')))
 app.use(cookieParser());
 
-const chat = io.of('/chat');
-app.set('io', io);
-chat.on('connection' , (socket)=>{
-    console.log('chat 네임스페이스 접속')
-	 
-	  socket.on("test", (data) => {
-	const  name = data.name;
-	const  msg = data.msg;
-  	
-	 socket.emit("test",{
-                name,
-                msg
-				})
+//몽고디비연결부분
+const connect = require("./schemas");
+connect();
 
-		 
-	  })
-	socket.on("test2", (data) =>{
-           console.log(data)
-       })
-       
-   
+//소켓 방.채팅창 로직부분
+app.set('io', io);
+const room = io.of('/room')
+const chat = io.of('/chat');
+
+
+room.on('connection', (socket) => {//게임방에 접속
+    console.log('게임방에 입장하셨습니다 ');
+    room.emit("notice", {
+        message: "게임에 참여하신걸 환영합니다"
+    })
+    socket.on('disconnect', () => {
+        console.log('게임방 접속해제')
+    })
+})
+
+chat.on('connection', (socket) => {
+    console.log('chat 네임스페이스 접속')
+
+    socket.on("test", (data) => {
+        const name = data.name;
+        const msg = data.msg;
+        
+        chat.emit("test", {
+            name: name,
+            msg: msg
+        })
+    })
+
+
 })
 
 
-server.listen( PORT , () =>{
+app.use("/", (req, res) => {
+    res.status(200).json("서버 정상 작동 합니다")
+})
+
+server.listen(PORT, () => {
     console.log(`PORT 번호 ${PORT} 으로 서버가 실행되었습니다!`)
 })
---
+++ schemas/chat.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const chatSchema = new Schema({
+
+    room: {
+        type: String,
+        required: true,
+    },
+    postId: {
+        type: String,
+        required: true,
+    },
+    chatLog: {
+        type: Array,
+        required: false,
+    },
+    nickname: {
+        type: Array,
+        required: false,
+    },
+    chatTime: {
+        type: Array,
+        required: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model(`Chat`, chatSchema);
